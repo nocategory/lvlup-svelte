54634bbb3fdceb47c815ea84cdf76a371ce208b3
João <joao@joao.codes>
2021-10-22 14:15:44 +0000

    questions

---
+++ src/Question.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let question
+
+  let answers = question.incorrect_answers
+</script>
+
+<h3>{question.question}</h3>
+
+{#each answers as answer}
+  <button>{answer}</button>
+{/each}
--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,14 +1,6 @@
 <script>
-  let result
-  let correctAnswer = 'c'
-  const answers = ['a', 'b', 'c', 'd']
+  import Question from './Question.svelte'
   let quiz = getQuiz()
-
-  function checkAnswer(answer) {
-    return answer === correctAnswer
-      ? (result = 'Correct!!!!')
-      : (result = 'Wrong DUH!')
-  }
 
   async function getQuiz() {
     const res = await fetch(
@@ -26,18 +18,10 @@
   {#await quiz}
     <h4>Loading...</h4>
   {:then data}
-    <h2>{data.results[0].question}</h2>
+    {#each data.results as question}
+      <Question {question}>
+        {question}
+      </Question>
+    {/each}
   {/await}
-
-  {#if result}
-    <h4>{result}</h4>
-  {:else}
-    <h4>Pick an answer!</h4>
-  {/if}
-
-  {#each answers as answer}
-    <button on:click={() => checkAnswer(answer)}
-      >Answer {answer.toUpperCase()}</button
-    >
-  {/each}
 </div>
